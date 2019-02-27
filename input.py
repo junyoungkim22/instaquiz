b60532a8c6d3861e00787b7e735e909d3efc5664
Junyoung Kim <junyoung2@gmail.com>
2019-02-27 11:18:57 +0900

    Make static sentence question generator intoseperate class

--- input.py
+++ input.py
@@ -1,36 +1,10 @@
-import nltk
-import re
-from textblob import TextBlob
-
-class Sentence_QG:
-    paragraph = ''
-    sent_list = []
-    def __init__(self, paragraph):
-        self.paragraph = paragraph
-        blob = TextBlob(self.paragraph)
-        for sent in blob.sentences:
-            self.sent_list.append(str(sent))
-            
-    def noun_question(self, sentence):
-        sent_blob = TextBlob(sentence)
-        noun_list = sent_blob.noun_phrases
-        question = sentence
-        for np in sent_blob.noun_phrases:
-            pattern = re.compile(np, re.IGNORECASE)
-            question = pattern.sub("_______", question)
-        return question
-
-    def question_generation(self):
-        question_list = []
-        for sent in self.sent_list:
-            question_list.append(self.noun_question(sent))
-        return question_list
+from static_sent_qg import StaticSentQG
 
 f = open("demo_text.txt")
 text = f.read()
 
-sent_qg = Sentence_QG(text)
+sent_qg = StaticSentQG(text)
 
-print sent_qg.question_generation()
-
-blob = TextBlob(text)
+for sent in sent_qg.question_generation():
+    print "-"*50
+    print sent
---
+++ static_sent_qg.py
@@ -0,0 +1,27 @@
+import nltk
+import re
+from textblob import TextBlob
+
+class StaticSentQG:
+    paragraph = ''
+    sent_list = []
+    def __init__(self, paragraph):
+        self.paragraph = paragraph
+        blob = TextBlob(self.paragraph)
+        for sent in blob.sentences:
+            self.sent_list.append(str(sent))
+            
+    def noun_question(self, sentence):
+        sent_blob = TextBlob(sentence)
+        noun_list = sent_blob.noun_phrases
+        question = sentence
+        for np in sent_blob.noun_phrases:
+            pattern = re.compile(np, re.IGNORECASE)
+            question = pattern.sub("_______", question)
+        return question
+
+    def question_generation(self):
+        question_list = []
+        for sent in self.sent_list:
+            question_list.append(self.noun_question(sent))
+        return question_list
