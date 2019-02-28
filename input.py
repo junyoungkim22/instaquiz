d512a5329c6d2569cacbcd80a37336e709403f04
junyoung2 <junyoung2@gmail.com>
2019-02-28 05:50:09 -0800

    add ml_para_qg.py with fake outputs

--- input.py
+++ input.py
@@ -1,16 +1,22 @@
 from static_sent_qg import StaticSentQG
 from ml_sent_qg import MlSentQG
+from ml_para_qg import MlParaQG
 
 f = open("demo_text.txt")
 text = f.read()
 
 static_sent_qg = StaticSentQG(text)
 ml_sent_qg = MlSentQG(text)
+ml_para_qg = MlParaQG(text)
 
 for sent in static_sent_qg.question_generation():
     print "-"*50
     print sent
     
 for sent in ml_sent_qg.question_generation()[0]:
+    print "-"*50
+    print sent
+
+for sent in ml_para_qg.question_generation()[0]:
     print "-"*50
     print sent
---
+++ ml_para_qg.py
@@ -0,0 +1,19 @@
+from textblob import TextBlob
+
+class MlParaQG():
+    paragraph = ''
+    sent_list = []
+
+    def __init__(self, paragraph):
+        self.paragraph = paragraph
+        blob = TextBlob(self.paragraph)
+        for sent in blob.sentences:
+            self.sent_list.append(str(sent))
+
+    def question_generation(self):
+        #Fake outputs
+        questions = []
+        answers = []
+        questions.append("What are three fields of study that are related to machine learning?")
+        answers.append("computational statistics, mathematical optimization, Data mining")
+        return (questions, answers)
