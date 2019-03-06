78f7b9b1771635570a1c9b00905fee949c473af7
junyoung2 <junyoung2@gmail.com>
2019-03-05 22:01:26 -0800

    Implement simple dictionary of 66202 words, acheive 80-90% hit rate on random paragraphs

--- ml_sent_qg/dict_generator.py
+++ ml_sent_qg/dict_generator.py
@@ -1,18 +1,20 @@
 import squad_loader
 import pickle
 
+dict_directory = "dictionary/"
+
 class WordIndexMapper:
     def __init__(self, w2i_filename, i2w_filename, w2c_filename):
-        self.w2i_filename = w2i_filename
-        self.i2w_filename = i2w_filename
-        self.w2c_filename = w2c_filename
-        w2i_pkl_file = open(w2i_filename, 'rb')
+        self.w2i_filename = dict_directory + w2i_filename
+        self.i2w_filename = dict_directory + i2w_filename
+        self.w2c_filename = dict_directory + w2c_filename
+        w2i_pkl_file = open(self.w2i_filename, 'rb')
         self.word2index = pickle.load(w2i_pkl_file)
         w2i_pkl_file.close()
-        i2w_pkl_file = open(i2w_filename, 'rb')
+        i2w_pkl_file = open(self.i2w_filename, 'rb')
         self.index2word = pickle.load(i2w_pkl_file)
         i2w_pkl_file.close()
-        w2c_pkl_file = open(w2c_filename, 'rb')
+        w2c_pkl_file = open(self.w2c_filename, 'rb')
         self.word2count = pickle.load(w2c_pkl_file)
         w2c_pkl_file.close()
         self.n_words = len(self.word2index)
@@ -45,7 +47,36 @@ class WordIndexMapper:
         pickle.dump(self.word2count, output)
         output.close()
 
-
-data = squad_loader.process_file("train-v2.0.json")
-
-
+    def paragraph_test(self, para):
+        word_count = 0
+        hit_count = 0
+        for sent in para.split(' '):
+            (hc, wc) = self.sentence_test(sent)
+            word_count += wc
+            hit_count += hc
+        print ("word count: ", word_count)
+        print ("hit count: ", hit_count)
+        print ("percentage: " + str((float(hit_count) / word_count) * 100) + "%") 
+        
+    def sentence_test(self, sent): 
+        word_count = 0 
+        hit_count = 0 
+        for word in sent.split(' '): 
+            word_count += 1 
+            if word in self.word2index: 
+                hit_count += 1 
+            return (hit_count, word_count) 
+                                
+data = squad_loader.process_file("train-v2.0.json") 
+i = 0 
+maker = WordIndexMapper("word_to_index.pkl", "index_to_word.pkl", "word_to_count.pkl") 
+for context, context_qas in data: 
+    if i < 6000:
+        maker.addParagraph(context) 
+        i += 1 
+    else:
+        maker.paragraph_test(context)
+        i += 1
+        if i == 6050:
+            break
+print(len(maker.word2index))
